b9df68ad85631b2d245de0ddf766a02b840f847d
Максим <umaks9168@mail.ru>
2026-01-08 18:13:57 +0700

    gg

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,12 +1,32 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+)
+
 from lexicon.lexicon import LEXICON_RU
 
 button_yes = KeyboardButton(text=LEXICON_RU["yes_button"])
 button_no = KeyboardButton(text=LEXICON_RU["no_button"])
-button_restart_help = KeyboardButton(text = LEXICON_RU["help_restart_button"])
+button_restart_help = KeyboardButton(text=LEXICON_RU["help_restart_button"])
 button_restart_yes = KeyboardButton(text=LEXICON_RU["yes_restart_button"])
 
+button_asmu = InlineKeyboardButton(text="АГМУ", callback_data="АГМУ")
+button_altgtu = InlineKeyboardButton(text="АлтГТУ", callback_data="АлтГТУ")
 
-yes_no = ReplyKeyboardMarkup(keyboard=[[button_yes,button_no]],one_time_keyboard=True, resize_keyboard=True)
+university_keyb = InlineKeyboardMarkup(
+    inline_keyboard=[[button_asmu], [button_altgtu]]
+)
 
-restart_keyb = ReplyKeyboardMarkup(keyboard =[[button_restart_yes,button_restart_help]],one_time_keyboard=True,resize_keyboard=True)
+yes_no = ReplyKeyboardMarkup(
+    keyboard=[[button_yes, button_no]],
+    one_time_keyboard=True,
+    resize_keyboard=True,
+)
+
+restart_keyb = ReplyKeyboardMarkup(
+    keyboard=[[button_restart_yes, button_restart_help]],
+    one_time_keyboard=True,
+    resize_keyboard=True,
+)
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import asyncio
 import logging
+
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram import Bot, Dispatcher
-from config.config import Config ,load_config
+
+from config.config import Config, load_config
 from handlers import other, user
 
 logger = logging.getLogger(__name__)
+
 
 async def main():
     config: Config = load_config()
     logging.basicConfig(
         level=logging.getLevelName(level=config.log.level),
-        format = config.log.format,
+        format=config.log.format,
     )
     logger.info("Starting bot")
-    bot = Bot(token=config.bot.token,default=DefaultBotProperties(parse_mode=ParseMode.HTML),)
+    bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
     dp = Dispatcher()
 
     dp.include_router(user.router)
@@ -23,5 +29,6 @@ async def main():
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 asyncio.run(main())
